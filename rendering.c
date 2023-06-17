6f6194a9ea8399e54f5c06104335a10bd4168767
chinedel.chinedu <chinedel.chinexeluwa@gmail.com>
2023-06-17 19:22:19 +0000

    rendering

--- rendering.c
+++ rendering.c
@@ -1,13 +1,21 @@
-fuhfkrljenfklvnaffre
-,fl,s
-rfmlvf
-mr
-frfm
-rfvrmfvr;f,le
-rf,
-fmrvrlmrlgv,r
-lvrvg,rlv
-rvrlf,rvrfe
-rf,ervr
-vmrvr
-rfrmv;rv
+#include <stdio.h>
+#include "rendering.h"
+#include "map.h"
+#include "game.h"
+
+void render_game() {
+    // Render the game based on the map
+    for (int z = 0; z < MAP_DEPTH; z++) {
+        for (int y = 0; y < MAP_HEIGHT; y++) {
+            for (int x = 0; x < MAP_WIDTH; x++) {
+                if (map_is_wall(x, y, z)) {
+                    printf("#");
+                } else {
+                    printf(".");
+                }
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+}
